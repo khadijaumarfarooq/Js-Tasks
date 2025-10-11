267816ca7a2e1fc75879d7aa2c4d257afadea8f5
khadijaumarfarooq <khankubra821@gmail.com>
2025-10-11 20:39:29 +0500

    Create Task4(part-1).js

---
+++ Task4(part-1).js
@@ -0,0 +1,10 @@
+function getEmail() {
+    let email = prompt("Enter Your Email");
+    if (email) {
+        alert("Your Email is:"+ email)
+    }
+    else {
+        alert("you didn't entered any email")
+    } 
+}
+getEmail()
